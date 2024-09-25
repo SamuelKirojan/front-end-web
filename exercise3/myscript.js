377706f29867b6339f01eedb536be4f32a17961f
Samuelkirojan <kirojansamuel@gmail.com>
2024-09-25 11:09:16 +0800

    commit pertama saya

---
+++ exercise3/myscript.js
@@ -0,0 +1,8 @@
+let header = (
+    <header className="header">
+        <h1>Buat Header menggunakan JSX</h1>
+        <p>tutorial JSX sederhana</p>
+    </header>
+);
+
+ReactDOM.render(header, document.getElementById("app"));
